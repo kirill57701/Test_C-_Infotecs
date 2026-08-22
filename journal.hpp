5d18f92fae189c7b95f426982a57bf1221e808d4
Kirill <kirillpetrov56333@gmail.com>
2026-08-22 12:23:16 +0300

    adding init + print journal

--- journal.hpp
+++ journal.hpp
@@ -1,10 +1,19 @@
 #ifndef JOURNAL_HPP
 #define JOURNAL_HPP
+#include <string>
+#include <iostream>
 
 struct journal {
   string s;
   int lvl;
   string imp;
+  journal(string name, int l) {
+    s = name;
+    lvl = lvl;
+  }
+  void print_info() {
+    cout << "name " << s << "\nlevel " << lvl;
+  }
 };
 
 #endif
