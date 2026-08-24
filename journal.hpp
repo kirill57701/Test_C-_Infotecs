55cbb8e85cf6e018b27a78b286952435d905015c
Kirill <kirillpetrov56333@gmail.com>
2026-08-24 06:02:09 +0300

    adding save file + change lvl

--- journal.hpp
+++ journal.hpp
@@ -12,7 +12,7 @@ struct journal {
   journal(std::string name, int l): s(name), lvl(l)
   {}
   void print_info() {
-    std::cout << "name " << s << "\nlevel " << lvl << "\nmessage " << txt << "\ntime " << t;
+    std::cout << "name " << s << "\nlevel " << lvl << "\nmessage " << txt << "\ntime " << t << "\n";
   }
   void save_mess(std::string mess, std::string time, int l) {
     if (l >= lvl) {
@@ -24,6 +24,27 @@ struct journal {
     } else {
       throw std::logic_error("err, lvl is small");
     }
+  }
+  void change_lvl(int new_lvl) {
+    if (new_lvl < lvl) {
+      throw std::logic_error("err, lvl small");
+    } else {
+      lvl = new_lvl;
+    }
+  }
+  void save_file(std::string s) {
+    std::ofstream out;
+    out.open(s);
+    out << "name: " << s << "\nlevel: " << lvl << "\nmessage: " << txt << "\ntime: " << t << "\n";
+    out.close();
+  }
+  void save_file() {
+    std::string s;
+    std::cin >> s;
+    std::ofstream out;
+    out.open(s);
+    out << "name: " << s << "\nlevel: " << lvl << "\nmessage: " << txt << "\ntime: " << t << "\n";
+    out.close();
   }
 };
 
