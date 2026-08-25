979178c8c94abb161b183ca0140a6856bec571e6
Kirill <kirillpetrov56333@gmail.com>
2026-08-25 14:00:31 +0300

    try adding sockets

--- journal.cpp
+++ journal.cpp
@@ -3,8 +3,19 @@
 #include <fstream>
 #include <ctime>
 #include "journal.hpp"
+#include <sys/socket.h>
 
 journal::journal(std::string name_init, int l_init): name(name_init), lvl(l_init){
+  if (lvl == 1) {
+    lvl_name = "usual";
+  } else if (lvl == 2) {
+    lvl_name = "attention";
+  } else {
+    lvl_name = "err";
+  }
+}
+
+journal::journal(std::string name_init, int l_init, std::string add_init, int serv_init): name(name_init), lvl(l_init), server(serv_init), addr(add_init){
   if (lvl == 1) {
     lvl_name = "usual";
   } else if (lvl == 2) {
@@ -34,7 +45,7 @@ void journal::save_mess(std::string mess, int l) {
     tm* timer = localtime(&t);
     std::ofstream out;
     out.open(name, std::ios::app);
-    out << "name: " << name << "\nlevel: " << lvl << "\nmessage: " << mess << "\ntime: " << timer->tm_hour << ':' << timer->tm_min << ':' << timer->tm_sec;
+    out << "name: " << name << "\nlevel: " << l << "\nmessage: " << mess << "\ntime: " << timer->tm_hour << ':' << timer->tm_min << ':' << timer->tm_sec;
     out.close();
   } else {
     throw std::logic_error("err, lvl is small");
--- journal.hpp
+++ journal.hpp
@@ -4,12 +4,16 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <sys/socket.h>
 
 struct journal {
   std::string name;
   int lvl;
   std::string lvl_name;
+  int server;
+  std::string addr;
   journal(std::string name_init, int l_init);
+  journal(std::string name_init, int l_init, std::string addr_init, int server);
   void save_mess(std::string mess, int l);
   void change_lvl(int new_lvl);
 };
