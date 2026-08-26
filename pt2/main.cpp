21e0813a4cb23692125559f3c5d43c44c3f8fb50
Kirill <kirillpetrov56333@gmail.com>
2026-08-26 03:29:53 +0300

    it works!

--- pt2/main.cpp
+++ pt2/main.cpp
@@ -3,26 +3,27 @@
 #include <thread>
 
 int main() {
-  std::string s, mess;
-  int l, l2;
+  std::string s;
+  int l;
   std::cin >> s >> l;
   journal master{s, l};
   /*
   std::cin >> mess >> l2;
   master.save_mess(mess, l2);
   */
+  std::string r;
+  int r1 = master.lvl;
   while (1) {
-    std::string r;
-    int r1;
+    int c;
     std::cin >> r;
-    if (r == "end") {
-      break;
-    } else if (std::cin.eof()) {
-      r1 = master.lvl;
-      std::thread t1(&journal::save_mess, &master, r, r1);
-      t1.join();
+    if (std::cin >> c) {
+      r1 = c;
     } else {
-      std::cin >> r1;
+      std::cin.clear();
+    }
+    if (std::cin.eof() || r == "end") {
+      break;
+    } else {
       std::thread t2(&journal::save_mess, &master, r, r1);
       t2.join();
     }
